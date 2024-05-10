cd1c3bb7fcaf8ff2d43a3d1388ea52cbdc326107
hammadprogrammer77 <154601362+hammadprogrammer77@users.noreply.github.com>
2024-05-10 22:06:40 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,71 @@
+
+var one = 10
+var two = "hello"
+var three = "world"
+
+console.log(one)
+console.log(two)
+console.log(three)
+
+
+var username = "hammad"
+
+console.log(username)
+
+
+
+
+
+// Legal variable names:
+var verables= "legal variables"
+
+document.write( "<h2>" + verables + "</h2>");
+
+
+
+var legalVariables = "age"
+
+document.write(legalVariables + "</br>" );
+
+var legalVariables = "username"
+
+document.write(legalVariables + "</br>");
+
+var legalVariables = "_hello"
+
+document.write(legalVariables + "</br>" );
+
+var legalVariables = "totalStudents" 
+
+document.write(legalVariables + "</br>" ); 
+
+var legalVariables = "Max_Value"
+
+document.write(legalVariables + "</br>" ); 
+
+
+
+
+// illlegal verables names
+
+var illlegal = "unlegal veriables"
+document.write( "<h2>" + illlegal + "</h2>" ); 
+
+
+var unlegalVeriables ="123number" 
+document.write(unlegalVeriables + "</br>" ); 
+
+var unlegalVeriables ="my variable" 
+document.write(unlegalVeriables + "</br>" );  
+
+var unlegalVeriables ="my-variable" 
+document.write(unlegalVeriables + "</br>" );  
+
+var unlegalVeriables ="total#amount" 
+document.write(unlegalVeriables + "</br>" ); 
+
+var unlegalVeriables ="class" 
+document.write(unlegalVeriables); 
+
+
+
